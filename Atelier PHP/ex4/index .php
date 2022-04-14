b1e0fd89c2e040d8b6481c0b8800c3d509d70bf9
Tasnim-Ferchichi <103393692+Tasnim-Ferchichi@users.noreply.github.com>
2022-04-15 00:01:03 +0200

    Add files via upload

---
+++ Atelier PHP/ex4/index .php
@@ -0,0 +1,44 @@
+<?php 
+session_start();
+if (!isset($_SESSION['notes'])){
+    $_SESSION['notes'] = [];
+}
+if (isset($_POST['titre']) && isset($_POST['note'])){
+    $_SESSION['notes'][] = ['titre' => $_POST['titre'], 'note' => $_POST['note']]; 
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Keep</title>
+    <style>
+        
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="notes-card">
+                <h3 class="notes-card-title">Notes</h3>
+                <?php
+                    foreach ($_SESSION['notes'] as $note):
+                ?>
+                    <div class="notes-card-item">
+                        <b><?= $note['titre'] ?></b>
+                        <div><?= $note['note'] ?></div>
+                    </div>
+                <?php
+                    endforeach
+                ?>
+            </div>
+            <form action="" method="POST">
+                <input type="text" class="input-field" name="titre" placeholder="Titre" required>
+                <textarea name="note" class="input-field" rows="2" placeholder="Note" required></textarea>
+                <button type="submit" class="btn">Ajouter</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
